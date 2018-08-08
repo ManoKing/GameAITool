928d2baeaaa9a6c1587bc0ec7e5596536fb5d477
mano <mano@mano.com>
2018-08-08 09:53:18 +0800

    增加屏幕适配CanvasManager脚本

--- Assets/Examples/TexturePacker/LoadTexture.cs
+++ Assets/Examples/TexturePacker/LoadTexture.cs
@@ -6,12 +6,26 @@ public class LoadTexture : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Sprite _sprite = TextureManage.getInstance().LoadAtlasSprite("Texture/Number", "1s7");
-        GetComponent<Image>().sprite = _sprite;
+        StartCoroutine(Number());
     }
-	
 	// Update is called once per frame
 	void Update () {
-		
+       
 	}
+    IEnumerator Number()
+    {
+        int i = 1;
+        while (true)
+        {
+            Sprite _sprite = TextureManage.getInstance().LoadAtlasSprite("Texture/Number", i.ToString());
+            GetComponent<Image>().sprite = _sprite;
+            GetComponent<Image>().SetNativeSize();
+            yield return new WaitForSeconds(0.1f);
+            i++;
+            if (i>49)
+            {
+                i = 1;
+            }
+        }   
+    }
 }
